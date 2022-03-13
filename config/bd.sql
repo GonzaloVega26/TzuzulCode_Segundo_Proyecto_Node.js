ade544957fd242b2b5680848d2a89cfeb79f7cc5
Sergio A. Gamarra <sergioagamarra@gmail.com>
2022-03-13 19:22:36 -0300

    BD

--- config/bd.sql
+++ config/bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-03-2022 a las 19:41:09
+-- Tiempo de generación: 13-03-2022 a las 23:22:11
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 8.0.8
 
@@ -24,26 +24,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `movies`
---
--- Error leyendo la estructura de la tabla segundo_proyecto_backend.movies: #1932 - Table 'segundo_proyecto_backend.movies' doesn't exist in engine
--- Error leyendo datos de la tabla segundo_proyecto_backend.movies: #1064 - Algo está equivocado en su sintax cerca 'FROM `segundo_proyecto_backend`.`movies`' en la linea 1
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `renta`
 --
--- Error leyendo la estructura de la tabla segundo_proyecto_backend.renta: #1932 - Table 'segundo_proyecto_backend.renta' doesn't exist in engine
--- Error leyendo datos de la tabla segundo_proyecto_backend.renta: #1064 - Algo está equivocado en su sintax cerca 'FROM `segundo_proyecto_backend`.`renta`' en la linea 1
 
--- --------------------------------------------------------
+CREATE TABLE `renta` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id_pelicula` int(11) DEFAULT NULL,
+  `fecha_renta` date DEFAULT NULL,
+  `fecha_dev` date DEFAULT NULL,
+  `fecha_real_dev` date NOT NULL,
+  `estado` tinyint(1) DEFAULT NULL,
+  `comisión` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Estructura de tabla para la tabla `users`
+-- Índices para tablas volcadas
 --
--- Error leyendo la estructura de la tabla segundo_proyecto_backend.users: #1932 - Table 'segundo_proyecto_backend.users' doesn't exist in engine
--- Error leyendo datos de la tabla segundo_proyecto_backend.users: #1064 - Algo está equivocado en su sintax cerca 'FROM `segundo_proyecto_backend`.`users`' en la linea 1
+
+--
+-- Indices de la tabla `renta`
+--
+ALTER TABLE `renta`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_pelicula` (`id_pelicula`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `renta`
+--
+ALTER TABLE `renta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `renta`
+--
+ALTER TABLE `renta`
+  ADD CONSTRAINT `renta_ibfk_1` FOREIGN KEY (`id_pelicula`) REFERENCES `movies` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `renta_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`idUser`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
